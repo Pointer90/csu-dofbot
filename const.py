27a38875c55b1a9b2277f5469686fe5d2a3ce8b7
Arseniy_Ubunty <mr.elisar@gmail.com>
2025-04-22 16:58:22 +0500

    add modules

---
+++ const.py
@@ -0,0 +1,13 @@
+IN1 = 20
+IN2 = 21
+IN3 = 19
+IN4 = 26
+ENA = 16
+ENB = 13
+
+CarSpeedControl = 80
+
+RX = 8      # Define UART RX pin
+SP = 23     # Define Servo Pin (SP)
+EP = 0      # Define Echo Pin (EP)
+TP = 1      # Define Trig Pin (TP)
