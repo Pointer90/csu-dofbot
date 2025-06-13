3e2afedf7463f86e3dde7c21ac2581d5ca7fc022
FelixZeves <snowtitan2002@gmail.com>
2025-06-13 16:06:35 +0500

    fix: pc_2

--- pc_2.py
+++ pc_2.py
@@ -1,11 +1,11 @@
 from modules.wifi import Wifi
 from modules.parser.parser import Parser
-from modules.movement import BaseCar
+from modules.movement import Car4WD
 
 from time import sleep
 
 sock = Wifi(1).connect('localhost')
-car = BaseCar()
+car = Car4WD()
 
 while True:
     data = sock.recv(2048)
